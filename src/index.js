516199296fa3425bb2a79db01d776b5c7cbd5583
Michał Górski <dajminazwe@gmail.com>
2023-08-21 21:52:57 +0200

    Update basic information

--- src/index.js
+++ src/index.js
@@ -44,9 +44,30 @@ export default function App() {
                 </div>
                 <div class='resume__right-container'>
                     <SectionComponent title='About me' />
-                    <p>I'm a software developer who believes that understanding the business needs is essential to create scaleable software.</p>
+                    <p>With many years of experience in the IT industry, I have honed my skills in developing and maintaining web applications on various complexity levels. My non-standard approach to leading teams involves inspiring proactiveness and fostering a positive team culture.</p>
+
+                        <p>As a Team Lead, I am passionate about empowering my team to collaborate effectively. My strong communication skills enable me to communicate complex technical concepts to non-technical stakeholders and align our work with business objectives.</p>
+
+                        <p>I am always looking for innovative ways to optimize team performance and continuously improve our processes. If you're looking for a Team Lead who can deliver high-quality solutions while fostering a positive and collaborative team culture, let's connect and explore how we can achieve your business goals together.</p>
                     <SectionComponent title='Employment history' />
-                    <TimelineComponent title='Software Engineer, Onewelcome' startYear='2018' endYear='current' location='Gdańsk' />
+                    <TimelineComponent title='Team Lead, OneWelcome' startYear='2022' endYear='present' location='Gdańsk/Remote' />
+                    <p>I oversee a team comprising four skilled frontend engineers, tasked with the following key responsibilities:</p>
+
+                    <ul>
+                    <li>Developing and maintaining an open-source, highly accessible, component library in alignment with UX-designed templates.</li>
+                    <li>Collaborating on the creation of UI for new products.</li>
+                    <li>Exploring innovative solutions through the development of Proof of Concepts (PoCs).</li>
+                    </ul>
+
+                    <p>In my role as a team lead, I assume various duties, including:</p>
+                    <ul>
+                    <li>Acting as the primary point of contact for product management.</li>
+                    <li>Strategizing and refining our workflow and tasks.</li>
+                    <li>Conceptualizing novel solutions and designs.</li>
+                    <li>Nurturing a cohesive engineering culture within the team.</li>
+                    </ul>
+
+                    <TimelineComponent title='Software Developer, OneWelcome' startYear='2018' endYear='2022' location='Gdańsk/Remote' />
                     <p>I was working with a product operating in the IAM space where I was mostly responsible for the engineering tasks,
                         such as creating new features, testing, and styling.
                     </p>
