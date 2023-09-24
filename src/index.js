4034b556e8a4d8e5ce402aaa5308a354e60661c8
gorsky <dajminazwe@gmail.com>
2023-09-24 12:19:36 +0200

    Update current job description

--- src/index.js
+++ src/index.js
@@ -84,21 +84,23 @@ export default function App() {
                     <p>I am always looking for innovative ways to optimize team performance and continuously improve our processes. If you're looking for a Team Lead who can deliver high-quality solutions while fostering a positive and collaborative team culture, let's connect and explore how we can achieve your business goals together.</p>
                     <SectionComponent title='Employment history' />
                     <TimelineComponent title='Team Lead, OneWelcome' startYear='2022' endYear='present' location='Gdańsk/Remote' />
+                    <p>I lead a team of four skilled frontend engineers, tasked with the following key responsibilities:</p>
                     <ChipComponent titles={['ReactJS', 'TypeScript', 'Cypress', 'Storybook']} />
-                    <p>I oversee a team comprising four skilled frontend engineers, tasked with the following key responsibilities:</p>
 
                     <ul>
-                        <li>Developing and maintaining an open-source, highly accessible, component library in alignment with UX-designed templates.</li>
-                        <li>Collaborating on the creation of UI for new products.</li>
-                        <li>Exploring innovative solutions through the development of Proof of Concepts (PoCs).</li>
+                        <li>Developing and maintaining an open-source, highly accessible, component library in alignment with UX-designers.</li>
+                        <li>Building scalable user interfaces using ReactJS, TypeScript in a micro-frontend architecture.</li>
+                        <li>Trying out new ideas by developing proof-of-concepts, like UI customisation strategy of SaaS products or event-driven architecture of micro-frontends.</li>
+                        <li>Owning, developing and maintaining production-running micro-frontend containers.</li>
+                        <li>Writing various types of tests, including unit, component, end-to-end, and visual tests.</li>
                     </ul>
 
-                    <p>In my role as a team lead, I assume various duties, including:</p>
+                    <p>As a team leader, I also do the following:</p>
                     <ul>
-                        <li>Acting as the primary point of contact for product management.</li>
-                        <li>Strategizing and refining our workflow and tasks.</li>
-                        <li>Conceptualizing novel solutions and designs.</li>
-                        <li>Nurturing a cohesive engineering culture within the team.</li>
+                        <li>Plan and refine our work processes and tasks.</li>
+                        <li>Come up with new solutions.</li>
+                        <li>Promote a strong engineering culture within the team.</li>
+                        <li>Handle tasks like conducting job interviews, 1-on-1s, and annual performance reviews.</li>
                     </ul>
 
                     <TimelineComponent title='Software Developer, OneWelcome' startYear='2018' endYear='2022' location='Gdańsk/Remote' />
